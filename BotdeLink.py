351c499ec18883003a6e88e2472dd8e8eec9da39
KaiqueBastos <85174224+KaiqueBastos@users.noreply.github.com>
2021-06-01 13:19:27 -0300

    Bot de link para as aulas

---
+++ BotdeLink.py
@@ -0,0 +1,47 @@
+#coding: utf-8
+import time 
+import pywhatkit 
+
+c = 1
+
+while (c > 0):
+    
+
+#segunda feira
+    #pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos, Link para a aula de FC - https://meet.google.com/ysf-giao-crz",7,35,10)
+    #pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de SOCIOLOGIA - https://meet.google.com/vxa-atyr-jba",8,15,10)
+    #pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL'," Aula iniciando em 5 minutos,link para a aula de Assistencia a informatica - https://meet.google.com/qpz-mhnb-cew",8,55,10)
+    #pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de RESTABELECIMENTO DE UMA ESTAÇÃO DO TRABALHO - https://meet.google.com/xxr-kabk-emxu ",10,35,10)
+
+#terça-feira
+    #pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL'," Aula iniciando em 5 minutos,link para a aula de OTIMIZAÇO DE UMA ESTAÇÃO DE TRABALHO - https://meet.google.com/mom-csuv-cts",7,55,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL'," Aula iniciando em 5 minutos,link para a aula de MATEMATICA - https://meet.google.com/bbc-uhki-wcj",8,55,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de QUIMICA - https://meet.google.com/yvf-qrmb-ymk ",10,35,10)
+
+#quarta-feira
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Redacao - https://meet.google.com/nfa-xdnm-cfp ",7,35,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de ED.Fisica - https://meet.google.com/hzn-smxe-maj ",8,15,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Matematica - https://meet.google.com/rkm-tnsx-ufc ",8,55,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de de Espanhol - https://meet.google.com/pop-nqnx-xhz ",9,55,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de de Geografia - https://meet.google.com/xkd-jpgz-qxo ",10,35,10)
+#quinta-feira
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Ingles - https://meet.google.com/zdy-rewz-cde ",7,35,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de PV - https://meet.google.com/vpd-cfwe-oqr ",8,15,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Fisica - https://meet.google.com/ead-yzvg-nsf ",8,55,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de PORTUGUES - https://meet.google.com/jiy-nvgf-oed ",10,35,10)
+
+#sexta feira
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Biologia - https://meet.google.com/xoa-irwi-tyw ",7,35,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Historia - https://meet.google.com/ptp-dekh-fxi ",8,55,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de RESTABELECIMENTO DE UMA ESTAÇÃO DO TRABALHO - https://meet.google.com/fzs-ffbh-mce ",10,35,10)
+    pywhatkit.sendwhatmsg_to_group('Dols4YDtcUQHZVFK8NOszL',"Aula iniciando em 5 minutos,link para a aula de Filosofia - https://meet.google.com/jkd-kirh-tkx ",11,15,10)
+    time.sleep(162000)
+  
+
+
+
+
+
+
+
+
